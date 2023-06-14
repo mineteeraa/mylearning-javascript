26f1cd19257f27ab1c708dfd7be1cd092af62a34
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-14 16:17:05 +0700

    ASSIGNMENT: Using if/else statements to check with average population
    JavaScript Fundamentals – Part 1 -> LECTURE: Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -42,7 +42,17 @@ console.log(description);
 
 // 05. Strings and Template Literals
 description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+/*
 console.log(description);
+*/
+
+// 06. Taking Decisions: if / else Statements
+// population = 13;
+if (population > 33) {
+    console.log(`${country}'s population is above average`);
+} else {
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
 
 
 // *****CODING CHALLENGE*****
