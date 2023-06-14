d4693b788a6411dbb4ab40fc818fdc5f4bf9a1e0
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-14 15:37:36 +0700

    CHALLENGE: Compare BMI
    JavaScript Fundamentals – Part 1 -> Coding Challenge #1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,6 @@
-// ASSIGNMENT - JavaScript Fundamentals – Part 1
+// JavaScript Fundamentals – Part 1
 
+// *****ASSIGNMENT*****
 // 01. Values and Variables
 const country = "Thailand";
 const continent = "Asia";
@@ -27,10 +28,40 @@ console.log("Language: " + language);
 */
 
 // 04. Basic Operators
+/*
 console.log("Thailand has a population in half of " + population / 2 + " million");
 population++
 console.log(population);
 console.log(population > 6);
 console.log(population < 33);
+*/
 let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+/*
 console.log(description);
+*/
+
+
+// *****CODING CHALLENGE*****
+// #1
+let massMark;
+let massJohn;
+let heightMark;
+let heightJohn;
+// ***TEST DATA***
+// **DATA 1**
+massMark = 78;
+heightMark = 1.69;
+massJohn = 92;
+heightJohn = 1.95;
+// **DATA 2**
+/* massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76; */
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+console.log("BMI of Mark is " + BMIMark);
+console.log("BMI of John is " + BMIJohn);
+// ***BONUS***
+let markHigherBMI = (BMIMark > BMIJohn);
+console.log("BMI of Mark is higher than BMI of John is " + markHigherBMI);
