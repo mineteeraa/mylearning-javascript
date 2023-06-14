cee1356f92e7a5a90e8ddac6c760a7f5945f337b
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-14 14:21:47 +0700

    ASSIGNMENT: Declare variables, set boolean value, Log to console, FIX: Population value
    JavaScript Fundamentals – Part 1 -> LECTURE: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,19 @@
-let country  = "Thailand";
-let continent = "Asia";
-let population = "71.75";
+// ASSIGNMENT - JavaScript Fundamentals – Part 1
 
+// 01. Values and Variables
+let country = "Thailand";
+let continent = "Asia";
+let population = 71.75;
+/*
 console.log("Country: " + country);
 console.log("Continent: " + continent);
 console.log("Population (in millions) : " + population);
+*/
+
+// 02. Data Types
+let isIsland = false;
+let language;
+console.log("Type of island: " + typeof isIsland);
+console.log("Type of population: " + typeof population);
+console.log("Type of country: " + typeof country);
+console.log("Type of language: " + typeof language);
