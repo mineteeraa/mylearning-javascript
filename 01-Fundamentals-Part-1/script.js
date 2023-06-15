6ac0be07af89c69cce18166b84ea298cdc3a2d98
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-15 14:32:07 +0700

    ASSIGNMENT: Predict the result of operations
    JavaScript Fundamentals – Part 1 -> LECTURE: Type Conversion and Coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -56,6 +56,13 @@ if (population > 33) {
 }
 */
 
+// 07. Type Conversion and Coercion
+console.log('9' - '5'); //4
+console.log('19' - '13' + '17'); //617
+console.log('19' - '13' + 17); //23
+console.log('123' < 57); //false
+console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
+
 
 // *****CODING CHALLENGE*****
 // #1
