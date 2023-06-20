f51d0bd7108bdbb905cd415ee33bef01aa4ebd55
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-20 18:11:07 +0700

    ASSIGNMENT: Call percentageOfWorld1 inside new function, FIX: Change return value
    JavaScript Fundamentals – Part 2 -> LECTURE: Functions Calling Other Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,7 +14,7 @@ const describeKorea = describeCountry("Korea", 51.39, "Seoul");
 // 02. Function Declarations vs. Expressions
 // Function Declaration
 function percentageOfWorld1(population) {
-    return ((population / 7900) * 100) + "%";
+    return (population / 7900) * 100;
 }
 const percentageChina1 = percentageOfWorld1(1441);
 const percentageThailand1 = percentageOfWorld1(71.75);
@@ -23,7 +23,7 @@ const percentageJapan1 = percentageOfWorld1(125.33);
 
 // Function Expression
 const percentageOfWorld2 = function (population) {
-    return ((population / 7900) * 100) + "%";
+    return (population / 7900) * 100;
 }
 const percentageChina2 = percentageOfWorld2(1441);
 const percentageThailand2 = percentageOfWorld2(71.75);
@@ -31,8 +31,18 @@ const percentageJapan2 = percentageOfWorld2(125.33);
 /* console.log(percentageChina2, percentageThailand2, percentageJapan2); */
 
 // 03. Arrow Functions
-const percentageOfWorld3 = population => ((population / 7900) * 100) + "%";
+const percentageOfWorld3 = population => (population / 7900) * 100;
 const percentageChina3 = percentageOfWorld3(1441);
 const percentageThailand3 = percentageOfWorld3(71.75);
 const percentageJapan3 = percentageOfWorld3(125.33);
-console.log(percentageChina3, percentageThailand3, percentageJapan3);
+/* console.log(percentageChina3, percentageThailand3, percentageJapan3); */
+
+// 04. Functions Calling Other Functions
+function describePopulation(country, population) {
+    const percentage = percentageOfWorld1(population);
+    const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+    console.log(description);
+}
+describePopulation("China", 1441);
+describePopulation("Thailand", 71.75);
+describePopulation("Japan", 125.33);
