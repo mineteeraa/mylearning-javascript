49f474395db93a0e4cafb99e5e14fab1d4f29728
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-20 17:27:34 +0700

    ASSIGNMENT: Using function declaration and expression
    JavaScript Fundamentals – Part 2 -> LECTURE: Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -9,4 +9,23 @@ function describeCountry(country, population, capitalCity) {
 const describeThailand = describeCountry("Thailand", 71.75, "Bangkok");
 const describeJapan = describeCountry("Japan", 125.33, "Tokyo");
 const describeKorea = describeCountry("Korea", 51.39, "Seoul");
-console.log(describeThailand + '\n' + describeJapan + '\n' + describeKorea);
+/* console.log(describeThailand + '\n' + describeJapan + '\n' + describeKorea); */
+
+// 02. Function Declarations vs. Expressions
+// Function Declaration
+function percentageOfWorld1(population) {
+    return ((population / 7900) * 100) + "%";
+}
+const populationChina1 = percentageOfWorld1(1441);
+const populationThailand1 = percentageOfWorld1(71.75);
+const populationJapan1 = percentageOfWorld1(125.33);
+console.log(populationChina1, populationThailand1, populationJapan1);
+
+// Function Expression
+const percentageOfWorld2 = function (population) {
+    return ((population / 7900) * 100) + "%";
+}
+const populationChina2 = percentageOfWorld2(1441);
+const populationThailand2 = percentageOfWorld2(71.75);
+const populationJapan2 = percentageOfWorld2(125.33);
+console.log(populationChina2, populationThailand2, populationJapan2);
