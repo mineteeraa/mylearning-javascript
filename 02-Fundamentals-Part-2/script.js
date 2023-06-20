5986fd3540165ed9d13186c005f71b3ba9981b42
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-20 17:39:04 +0700

    ASSIGNMENT: Recreate function by using Arrow function, FIX: Change name of variables
    JavaScript Fundamentals – Part 2 -> LECTURE: Arrow Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -16,16 +16,23 @@ const describeKorea = describeCountry("Korea", 51.39, "Seoul");
 function percentageOfWorld1(population) {
     return ((population / 7900) * 100) + "%";
 }
-const populationChina1 = percentageOfWorld1(1441);
-const populationThailand1 = percentageOfWorld1(71.75);
-const populationJapan1 = percentageOfWorld1(125.33);
-console.log(populationChina1, populationThailand1, populationJapan1);
+const percentageChina1 = percentageOfWorld1(1441);
+const percentageThailand1 = percentageOfWorld1(71.75);
+const percentageJapan1 = percentageOfWorld1(125.33);
+/* console.log(percentageChina1, percentageThailand1, percentageJapan1); */
 
 // Function Expression
 const percentageOfWorld2 = function (population) {
     return ((population / 7900) * 100) + "%";
 }
-const populationChina2 = percentageOfWorld2(1441);
-const populationThailand2 = percentageOfWorld2(71.75);
-const populationJapan2 = percentageOfWorld2(125.33);
-console.log(populationChina2, populationThailand2, populationJapan2);
+const percentageChina2 = percentageOfWorld2(1441);
+const percentageThailand2 = percentageOfWorld2(71.75);
+const percentageJapan2 = percentageOfWorld2(125.33);
+/* console.log(percentageChina2, percentageThailand2, percentageJapan2); */
+
+// 03. Arrow Functions
+const percentageOfWorld3 = population => ((population / 7900) * 100) + "%";
+const percentageChina3 = percentageOfWorld3(1441);
+const percentageThailand3 = percentageOfWorld3(71.75);
+const percentageJapan3 = percentageOfWorld3(125.33);
+console.log(percentageChina3, percentageThailand3, percentageJapan3);
