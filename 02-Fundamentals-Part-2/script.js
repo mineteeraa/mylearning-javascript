09c9eb5003da6816324ecd2e409f724e88ef36ef
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-22 22:19:24 +0700

    CHALLENGE: Calculate and compare BMI by using object
    JavaScript Fundamentals – Part 2 -> Coding Challenge #3

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -102,8 +102,8 @@ const myCountry2 = {
         return this.isIsland = this.neighbours.length === 0 ? true : false;
     }
 }
-myCountry2.describe();
-console.log(myCountry2.checkIsland());
+/* myCountry2.describe();
+console.log(myCountry2.checkIsland()); */
 
 
 // *****CODING CHALLENGE*****
@@ -151,3 +151,30 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 // ***BONUS***
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 /* console.log(total); */
+
+// #3
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`);
+} else if (mark.calcBMI() < john.calcBMI()) {
+    console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`);
+}
