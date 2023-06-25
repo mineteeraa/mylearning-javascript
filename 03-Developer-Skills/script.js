afa5cc739c15bc8e0ce76ad3e9d9992eecf09e2c
mineteeraa <patteera_mu@cmu.ac.th>
2023-06-25 16:36:52 +0700

    CHALLENGE: Create function to print forecast temp each day
    Developer Skills & Editor Setup -> Coding Challenge #1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,22 @@
-// Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+// Developer Skills & Editor Setup
 
+// *****CODING CHALLENGE*****
+// #1
+function printForecast(arr) {
+    let text = '';
+    for (let i = 0; i < arr.length; i++) {
+        text = text + `... ${arr[i]}ºC in ${[i + 1]} days `;
+        if (i == arr.length - 1) {
+            text = text + '...';
+        }
+    }
+    console.log(text);
+}
+// ***TEST DATA***
+// **DATA 1**
+const forecastMaxTemp1 = [17, 21, 23];
+printForecast(forecastMaxTemp1);
+// **DATA 2**
+const forecastMaxTemp2 = [12, 5, -5, 0, 4];
+printForecast(forecastMaxTemp2);
